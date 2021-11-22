866813e385a89eba7d6cee530770e815a3e40610
ramil <ramil>
2021-11-22 17:20:18 +0000

    Reworked API. Given functions more meaninful names.

--- pluk_parser.ml
+++ pluk_parser.ml
@@ -2,14 +2,14 @@ module Stream = struct
   type 'a t = ('a stream_result) Lazy.t * int
 
   and 'a stream_result =
-    | Item of 'a * 'a t
-    | Eof
+    | Value of 'a * 'a t
+    | End
 
   let of_seq seq =
     let rec aux seq i () =
       match seq () with
-      | Seq.Cons (a, seq) -> Item (a, (Lazy.from_fun (aux seq (i + 1)), i))
-      | Seq.Nil -> Eof in
+      | Seq.Cons (a, seq) -> Value (a, (Lazy.from_fun (aux seq (i + 1)), i))
+      | Seq.Nil -> End in
     Lazy.from_fun (aux seq 1), 0
 
   let next ((lazy proc, _) : 'a t) = proc
@@ -22,41 +22,87 @@ module Stream = struct
       if !i < n then
         let a = Bytes.get bytes !i in
         let () = incr i in
-        Item (a, ((Lazy.from_fun next), !i))
+        Value (a, ((Lazy.from_fun next), !i))
       else
-        Eof in
+        End in
     Lazy.from_fun next, 0
 
   let of_string str = of_bytes (Bytes.of_string str)
 end
 
+type msg = ..
+
+type msg +=
+  | Eof
+  | Invalid_digit_character
+  | Invalid_digit
+  | Invalid_number_character
+  | Exact_item_mismatch
+  | String_mismatch
+  | Inexpected_end_of_stream
+  | Satisfy_mismatch
+  | Item_mismatch
+  | Stream_not_ended
+
 type 'a stream = 'a Stream.t
 
-type ('a, 's) item = 'a * 's stream
+type ('a, 's) elt = 'a * 's stream
 
-type 's error = string * 's stream
+type 's error = (msg, 's) elt
 
-type ('a, 's) parse_result = (('a, 's) item, 's error) result
+type ('a, 's) parse_result = (('a, 's) elt, 's error) result
 
 type ('a, 's) t = 's stream -> ('a, 's) parse_result
 
 let apply stream parser = parser stream
 
-let next parser proc stream =
-  match (parser stream) with
-  | Ok (x, s1) -> proc x s1
-  | Error (err, s1) -> Error (err, s1)
+let map proc = function
+  | Ok (a, stream) -> Ok (proc a, stream)
+  | Error e -> Error e
+
+let map_error proc = function
+  | Ok a -> Ok a
+  | Error (e, stream) -> Error (proc e, stream)
+
+let bind a proc =
+  match a with
+  | Ok a -> proc a
+  | Error e -> Error e
+
+let ok a stream = Ok (a, stream)
 
 let error err stream = Error (err, stream)
 
+let map_value proc parser stream = map proc (parser stream)
+
+let ok_unit stream = ok () stream
+
+let next parser proc stream =
+  match (parser stream) with
+  | Ok (a, stream') -> proc a stream'
+  | Error e -> Error e
+
+let string_of_msg = function
+  | Eof -> Some "Eof"
+  | Invalid_digit_character -> Some "Invalid_digit_character"
+  | Invalid_digit -> Some "Invalid_digit"
+  | Invalid_number_character -> Some "Invalid_number_character"
+  | Exact_item_mismatch -> Some "Exact_item_mismatch"
+  | String_mismatch -> Some "String_mismatch"
+  | Inexpected_end_of_stream -> Some "Inexpected_end_of_stream"
+  | Satisfy_mismatch -> Some "Satisfy_mismatch"
+  | Item_mismatch -> Some "Item_mismatch"
+  | Stream_not_ended -> Some "Stream_not_ended"
+  | _ -> None
+
 let any_item stream =
   match Stream.next stream with
-  | Stream.Item (x, s1) -> Ok (x, s1)
-  | Stream.Eof -> Error ("eof", stream)
+  | Stream.Value (x, s1) -> Ok (x, s1)
+  | Stream.End -> Error (Eof, stream)
 
-let some_of parsers =
+let or_ parsers =
   match parsers with
-  | [] -> failwith "Empty parsers list"
+  | [] -> invalid_arg "Empty parsers list"
   | p :: ps ->
       fun stream ->
         let rec parse parser parsers =
@@ -71,121 +117,113 @@ let some_of parsers =
         parse p ps
 
 let zero_or_many init parser stream =
-  let rec loop x s =
-    match parser x s with
-    | Ok (x, s1) -> loop x s1
-    | Error _ -> Ok (x, s) in
+  let rec loop a stream =
+    match parser a stream with
+    | Ok (a, stream') -> loop a stream'
+    | Error _ -> Ok (a, stream) in
   loop init stream
 
-let one_or_many init parser =
-  next (parser init) (fun x -> zero_or_many x parser)
+let one_or_many init parser stream =
+  bind (parser init stream) @@ fun (a, stream) ->
+    zero_or_many a parser stream
 
 let digit_of_char =
   let range = [(0, Char.code '0', Char.code '9');
                (10, Char.code 'a', Char.code 'z');
                (10, Char.code 'A', Char.code 'Z')] in
-  fun x radix ->
-    let rec loop x range =
+  fun a radix ->
+    let rec loop a range =
       match range with
-      | (i, s, e) :: range ->
-          if x >= s && x <= e then begin
-            let dig = (x - s) + i in
-            if dig < radix then
-              Some ((x - s) + i)
+      | (base, range_a, range_b) :: range ->
+          if a >= range_a && a <= range_b then begin
+            let digit = (a - range_a) + base in
+            if digit < radix then
+              Some ((a - range_a) + base)
             else
               None
           end else
-            loop x range
+            loop a range
       | [] -> None in
-    loop x range
+    loop a range
 
 let digit radix stream =
   match Stream.next stream with
-  | Stream.Item (x, s1) ->
-      begin
-        match digit_of_char (Char.code x) radix with
-        | Some dig -> Ok (dig, s1)
-        | None -> Error ("parse_digit", s1)
+  | Stream.Value (a, stream') ->
+      begin match digit_of_char (Char.code a) radix with
+      | Some digit -> Ok (digit, stream')
+      | None -> Error (Invalid_digit_character, stream')
       end
-  | _ -> Error ("parse_digit", stream)
+  | _ -> Error (Invalid_digit, stream)
 
 let number radix =
-  one_or_many 0 (fun x ->
-    next (digit radix) (fun dig s1 ->
-      Ok ((x * radix) + dig, s1)))
+  one_or_many 0 (fun a ->
+    next (digit radix) (fun digit stream ->
+      Ok ((a * radix) + digit, stream)))
 
 let number_n radix len stream =
-  let proc (i, x) s =
+  let proc (i, x) stream =
     if i < len then begin
-      apply s (next (digit radix) (fun dig s1 ->
-        Ok ((i + 1, ((x * radix) + dig)), s1)))
+      apply stream (next (digit radix) (fun digit stream' ->
+        Ok ((i + 1, ((x * radix) + digit)), stream')))
     end else
-      Error ("parse_number_n", s) in
-  apply stream (next (one_or_many (0, 0) proc) (fun (_, x) s1 ->
-    Ok (x, s1)))
+      Error (Invalid_number_character, stream) in
+
+  apply stream (next (one_or_many (0, 0) proc) (fun (_, a) stream' ->
+    Ok (a, stream')))
 
 let exact_item item stream =
   match Stream.next stream with
-  | Stream.Item (x, s1) when (x = item) -> Ok (x, s1)
-  | _ -> Error ("exact_item", stream)
+  | Stream.Value (a, stream') when (a = item) -> Ok (a, stream')
+  | _ -> Error (Exact_item_mismatch, stream)
 
 let match_string str test stream =
   let n = String.length str in
-  let proc i s =
+  let proc i stream =
     if i < n then
-      match Stream.next s with
-      | Stream.Item (x, s1) when (test x (String.get str i)) -> Ok (i + 1, s1)
-      | Stream.Item (_, s1) -> Error ("match_string", s1)
-      | _ -> Error ("match_string premature end", s)
+      match Stream.next stream with
+      | Stream.Value (a, stream') when (test a (String.get str i)) ->
+          Ok (i + 1, stream')
+      | Stream.Value (_, stream') -> Error (String_mismatch, stream')
+      | _ -> Error (Inexpected_end_of_stream, stream)
     else
-      Error ("match_string", s) in
+      Error (String_mismatch, stream) in
   match zero_or_many 0 proc stream with
-  | Ok (i, s1) when (i = n) -> Ok (str, s1)
-  | Ok (_, s1) -> Error ("match_string", s1)
-  | Error (err, s1) -> Error (err, s1)
+  | Ok (i, stream') when (i = n) -> Ok (str, stream')
+  | Ok (_, stream') -> Error (String_mismatch, stream')
+  | Error err -> Error err
 
 let exact_string str = match_string str (=)
 
 let satisfy n_or_many test stream =
-  let proc a s =
-    match Stream.next s with
-    | Stream.Item (x, s1) when (test x) -> Ok (a, s1)
-    | _ -> Error ("match_if", s) in
+  let proc a stream =
+    match Stream.next stream with
+    | Stream.Value (b, stream') when test b -> Ok (a, stream')
+    | _ -> Error (Satisfy_mismatch, stream) in
+
   match n_or_many () proc stream with
-  | Ok (a, s1) -> Ok (a, s1)
-  | Error (err, s1) -> Error (err, s1)
+  | Ok (a, stream') -> Ok (a, stream')
+  | Error err -> Error err
 
 let satisfies test stream =
   match Stream.next stream with
-  | Stream.Item (x, s1) when (test x) -> Ok (x, s1)
-  | _ -> Error ("match_item", stream)
+  | Stream.Value (a, stream') when test a -> Ok (a, stream')
+  | _ -> Error (Item_mismatch, stream)
 
-let string_of parser stream =
+let into_string parser stream =
   let buf = Buffer.create 16 in
-  let rec loop s =
-    match (parser s) with
-    | Ok (ch, s1) ->
+  let rec loop stream =
+    match (parser stream) with
+    | Ok (ch, stream') ->
         Buffer.add_char buf ch;
-        loop s1
+        loop stream'
     | Error _ when ((Buffer.length buf) > 0) ->
-        Ok (Buffer.contents buf, s)
-    | Error (err, s1) -> Error (err, s1) in
+        Ok (Buffer.contents buf, stream)
+    | Error err -> Error err in
   loop stream
 
-let string_if test stream = string_of (satisfies test) stream
-
-let convert proc parser stream =
-  match (parser stream) with
-  | Ok (x, s1) -> Ok (proc x, s1)
-  | Error (err, s1) -> Error (err, s1)
-
-let void parser stream = convert ignore parser stream
-
-let const x stream = Ok (x, stream)
-
-let const_unit stream = const () stream
+let into_string_if test stream = into_string (satisfies test) stream
 
 let end_of_stream stream =
   match Stream.next stream with
-  | Stream.Item (_, s) -> Error ("end_of_stream", s)
-  | Stream.Eof -> Ok ((), stream)
+  | Stream.Value (_, stream') -> Error (Stream_not_ended, stream')
+  | Stream.End -> Ok ((), stream)
--- pluk_parser.mli
+++ pluk_parser.mli
@@ -1,29 +1,106 @@
+(** Simple combinatorial parser *)
+
+(**
+  Stream is a source of data for parser. Each instance of {!Stream.t} holds on
+  a specific position in underlying source. Multiple invocation of
+  {!Stream.next} on some {!Stream.t} instance always returns the same result.
+*)
 module Stream : sig
   type 'a t
-  and 'a stream_result = Item of 'a * 'a t | Eof
-  val of_seq : 'a Seq.t -> 'a t
+  (** The type for stream *)
+
+  and 'a stream_result =
+    | Value of 'a * 'a t
+    | End
+  (** The type for result of {!next}. *)
+
   val next : 'a t -> 'a stream_result
+  (** [next stream] returns the value from [stream] and instance of
+      stream positioned at next value *)
+
   val position : 'a t -> int
+  (** [position stream] returns the position of [stream] *)
+
+  val of_seq : 'a Seq.t -> 'a t
+  (** [of_seq seq] creates a stream using [seq] as a source. *)
+
   val of_bytes : bytes -> char t
+  (** [of_bytes bytes] creates a stream using [bytes] as a source. *)
+
   val of_string : string -> char t
+  (** [of_string string] creates a stream using [string] as a source. *)
 end
 
 type 'a stream = 'a Stream.t
-type ('a, 's) item = 'a * 's stream
-type 's error = string * 's stream
-type ('a, 's) parse_result = (('a, 's) item, 's error) result
+
+type ('a, 's) elt = 'a * 's stream
+
+type msg = ..
+(** The type for error messages. *)
+
+type msg +=
+  | Eof
+  | Invalid_digit_character
+  | Invalid_digit
+  | Invalid_number_character
+  | Exact_item_mismatch
+  | String_mismatch
+  | Inexpected_end_of_stream
+  | Satisfy_mismatch
+  | Item_mismatch
+  | Stream_not_ended
+(** Extended type for error messages returned by the library functions. *)
+
+type 's error = (msg, 's) elt
+
+type ('a, 's) parse_result = (('a, 's) elt, 's error) result
+(** Type for return value of each parser *)
+
 type ('a, 's) t = 's stream -> ('a, 's) parse_result
+(** Type of parser *)
 
 val apply : 's stream -> ('a, 's) t -> ('a, 's) parse_result
+(** [apply stream parser] returns the result of applying [parser] on [stream] *)
 
-val next : ('a, 's) t -> ('a -> 's stream -> ('b, 's) parse_result)
-           -> 's stream -> ('b, 's) parse_result
+val ok : 'a -> 'b stream -> ('a, 'b) parse_result
 
-val error : string -> 's stream -> ('a, 's) parse_result
+val error : msg -> 's stream -> ('a, 's) parse_result
+
+val string_of_msg: msg -> string option
+
+val map: ('a -> 'b) -> ('a, 's) parse_result -> ('b, 's) parse_result
+
+val map_error: (msg -> msg) -> ('a, 's) parse_result -> ('a, 's) parse_result
+
+val bind:
+  ('a, 's) parse_result ->
+  (('a * 's stream) -> ('b, 's) parse_result) ->
+  ('b, 's) parse_result
+
+val map_value :
+  ('a -> 'b) -> ('c -> ('a, 'd) parse_result) -> 'c -> ('b, 'd) parse_result
+
+val ok_unit : 'a stream -> (unit, 'a) parse_result
+
+val next :
+  ('a, 's) t ->
+  ('a -> 's stream -> ('b, 's) parse_result) ->
+  's stream ->
+  ('b, 's) parse_result
+(** [next parser proc stream] applies [proc] on result of [apply parse stream].
+  It is used for parser chaining:
+    {[
+      let parse_key_value =
+        next parse_word @@ fun key ->
+          next parse_delimiter @@ fun () ->
+            next parse_value @@ fun value stream' ->
+              Ok ((key, value), stream')
+    ]}
+ *)
 
 val any_item : 'a stream -> ('a, 'a) parse_result
 
-val some_of :
+val or_ :
   ('a stream -> ('b, 'a) parse_result) list ->
   'a stream -> ('b, 'a) parse_result
 
@@ -60,19 +137,12 @@ val satisfy :
 
 val satisfies : ('a -> bool) -> 'a stream -> ('a, 'a) parse_result
 
-val string_of :
+val into_string :
   ('a stream -> (char, 'a) parse_result) ->
-  'a stream -> (string, 'a) parse_result
+  'a stream ->
+  (string, 'a) parse_result
 
-val string_if : (char -> bool) -> char stream -> (string, char) parse_result
-
-val convert :
-  ('a -> 'b) -> ('c -> ('a, 'd) parse_result) -> 'c -> ('b, 'd) parse_result
-
-val void : ('a -> ('b, 'c) parse_result) -> 'a -> (unit, 'c) parse_result
-
-val const : 'a -> 'b stream -> ('a, 'b) parse_result
-
-val const_unit : 'a stream -> (unit, 'a) parse_result
+val into_string_if :
+  (char -> bool) -> char stream -> (string, char) parse_result
 
 val end_of_stream : 'a stream -> (unit, 'a) parse_result
